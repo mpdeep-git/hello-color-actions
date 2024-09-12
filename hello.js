75f2b30fec1635e62b2efe08b87046d8e3f8f81f
Pradeep Manoharan <pradeep.manoharan.iimb@gmail.com>
2024-09-12 06:01:14 -0400

    my first commit !

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from Jacksonville, Florida !!!";
 console.log(hello());
 exports.hello = hello;
 
